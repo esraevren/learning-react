646ccb9b679ac9ddcc1280ee3692d472e44920cf
Esra Emine Evren <88592010+esraevren@users.noreply.github.com>
2023-02-21 16:50:21 +0300

    first

---
+++ src/ContactList.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const ContactList = ({contacts, selectedId,dispatch}) => {
+  return (
+    <section className="contact-list">
+  <ul>
+  {contacts.map((contact)=>(
+    <li key={contact.id}>
+      <button onClick={()=> {
+        dispatch({type: 'changed_selection', contactId:contact.id})
+      }} 
+      >{selectedId === contact.id ? <b>{contact.name}</b>: contact.name }</button>
+    </li>
+    ))}
+  </ul>
+
+    </section>
+  )
+}
+
+export default ContactList
